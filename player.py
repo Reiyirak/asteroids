b0d61f6fed70a60f769715a15dee111c93e1f48f
Reiyirak <pdsmej@gmail.com>
2025-08-23 22:02:11 -0600

    Pygame: Groups

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
